54c85fe82072f955b3e05ceb20c290f3ba626405
Jonathan RIVO <rivo.jonathan98@gmail.com>
2022-04-11 16:30:01 +0200

    Started back office

---
+++ doctorBackOffice/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DoctorbackofficeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'doctorBackOffice'
---
+++ doctorBackOffice/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('appointment/<int:id>', views.appointment, name='appointment'),
+    path('/bill/<int:id>', views.bill, name='bill'),
+]
